bfada5890898749ddb5ce56f687abc0e22e8c654
miles <vigogiv@hotmail.com>
2015-04-01 17:32:40 +0800

    登录

--- src/main/java/com/keitsen/mvcproject/controller/UserController.java
+++ src/main/java/com/keitsen/mvcproject/controller/UserController.java
@@ -1,7 +1,12 @@
 package com.keitsen.mvcproject.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.keitsen.mvcproject.model.User;
 
 @Controller
 @RequestMapping("/")
@@ -10,5 +15,11 @@ public class UserController {
 	@RequestMapping("index")
 	public String index(){
 		return "index";
+	}
+	
+	@RequestMapping("login")
+	public String login(User user, HttpServletRequest request, HttpServletResponse response){
+		System.out.println(user.getUsername());
+		return "success";
 	}
 }
